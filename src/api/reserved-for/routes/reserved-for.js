e0ba7aad6263e07c9939dc316ae4b08b5038e887
Dane <darpera@yahoo.com>
2022-12-06 12:23:31 +0800

    minor changes

---
+++ src/api/reserved-for/routes/reserved-for.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reserved-for router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::reserved-for.reserved-for');
---
+++ src/api/reserved-for/services/reserved-for.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reserved-for service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::reserved-for.reserved-for');
